f4f98dfe2b5da68d9c743d78e765106789764f25
Zaibaa <zaibaahoosen@gmail.com>
2026-03-06 17:13:43 +0200

    Updated useEffect Dependency

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
-import { CartProvider } from "./context/CartContext";
+
 
 import './index.css';
 import App from './App';
